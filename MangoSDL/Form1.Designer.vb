83a7077171c006ba4af725460e8309bb588f1946
Md. Ashraful Islam <imdashraful17@gmail.com>
2024-01-12 17:52:47 +0600

    Add project files.

---
+++ MangoSDL/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtProjectFolder = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Vineta BT", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(535, 81)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "MangoSDL"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(27, 94)
+        Me.Label2.MaximumSize = New System.Drawing.Size(700, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(681, 100)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Create your CodeBlocks SDL Project from scratch. No hassle of downloading, and co" &
+    "nfiguring SDL libraries. All is there ready for you. Let's get started." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Md Ashr" &
+    "aful Islam © 2024 ALL RIGHTS RESERVED" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 255)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Location"
+        '
+        'txtProjectFolder
+        '
+        Me.txtProjectFolder.Location = New System.Drawing.Point(29, 277)
+        Me.txtProjectFolder.Name = "txtProjectFolder"
+        Me.txtProjectFolder.ReadOnly = True
+        Me.txtProjectFolder.Size = New System.Drawing.Size(320, 22)
+        Me.txtProjectFolder.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(356, 277)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(27, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Font = New System.Drawing.Font("Comic Sans MS", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(415, 255)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(344, 84)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "CREATE"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 397)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtProjectFolder)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MangoSDL"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtProjectFolder As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ MangoSDL/Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports Microsoft.Win32
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Reflection
+
+Public Class Form1
+    Dim CodeBlocksPath As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim key As RegistryKey = Registry.CurrentUser.OpenSubKey("Software\CodeBlocks")
+
+        If key IsNot Nothing Then
+            CodeBlocksPath = key.GetValue("Path")
+        Else
+            MsgBox("CodeBlocks is not installed on your computer. Install CodeBlocks first and then try again",
+                vbCritical, "Error")
+            End
+        End If
+
+
+    End Sub
+
+    Sub ExtractToPath(Path As String)
+        If Directory.GetFiles(Path).Count <> 0 Then
+            Throw New Exception("Please choose an empty folder")
+        End If
+
+        Dim ExeAsm As Assembly = Assembly.GetExecutingAssembly()
+        Using stream As Stream = ExeAsm.GetManifestResourceStream("MangoSDL.SDL Starter.zip")
+            Dim Template As New ZipArchive(stream)
+            Template.ExtractToDirectory(Path)
+        End Using
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            ExtractToPath(txtProjectFolder.Text)
+            If MsgBox("Project Successfully Created. Open it in CodeBlocks?", vbQuestion + vbYesNo, "Question") = vbYes Then
+                Dim command As String = CodeBlocksPath & "\codeblocks.exe """ & txtProjectFolder.Text & "\SDL Starter.cbp"" --file=""" & txtProjectFolder.Text & "\main.c"":9"
+                'Clipboard.Clear()
+                'Clipboard.SetText(command)
+                'MsgBox(command)
+                Shell(command, , False)
+                End
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Using ofolder As New FolderBrowserDialog()
+Again:
+            If ofolder.ShowDialog() = DialogResult.OK Then
+                If Directory.GetFiles(ofolder.SelectedPath).Count <> 0 Then
+                    MsgBox("Please select an empty folder.", vbOK + vbInformation, "Info")
+                    GoTo Again
+                End If
+                txtProjectFolder.Text = ofolder.SelectedPath
+            End If
+        End Using
+    End Sub
+
+    Private Sub txtProjectFolder_TextChanged(sender As Object, e As EventArgs) Handles txtProjectFolder.TextChanged
+        If txtProjectFolder.Text = "" Then
+            Button2.Enabled = False
+        Else
+            Button2.Enabled = True
+        End If
+    End Sub
+End Class
